199b5f3ab0115c1cca2752d897ce3c4794242196
Andre Mendes <andre.f.mendes24@gmail.com>
2022-12-02 18:51:02 +0000

    Reduced solution even further

--- 01-go/main.go
+++ 01-go/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -36,38 +37,23 @@ func elvesFromFile(filePath string) []int {
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
+	sort.SliceStable(foodItems, func(i, j int) bool { return foodItems[i] > foodItems[j] })
 	return foodItems
 }
 
-func findMaxElf(elves []int) (elf, maxTotal int) {
-	elf = 0
-	maxTotal = math.MinInt
-	for key, val := range elves {
-		if val > maxTotal {
-			elf = key
-			maxTotal = val
-		}
-	}
-	return elf, maxTotal
+func findMaxElf(elves []int) (maxTotal int) {
+	return elves[0]
 }
 
-func solvePartOne(filePath string) (elf, result int) {
+func solvePartOne(filePath string) (result int) {
 	elves := elvesFromFile(filePath)
 	return findMaxElf(elves)
-}
-
-func remove(s []int, index int) []int {
-	ret := make([]int, 0)
-	ret = append(ret, s[:index]...)
-	return append(ret, s[index+1:]...)
 }
 
 func findTopNElves(elves []int, n int) (maxTotal int) {
 	newN := int(math.Min(float64(n), float64(len(elves))))
 	for i := 0; i < newN; i++ {
-		elf, total := findMaxElf(elves)
-		maxTotal += total
-		elves = remove(elves, elf)
+		maxTotal += elves[i]
 	}
 	return maxTotal
 }
