4712f44fd5b42f6f65ca2d338de2839ad4f6b690
Andre Mendes <andre.f.mendes24@gmail.com>
2022-12-05 17:41:43 +0000

    Moved Stack type to data.go

--- 05-go/data.go
+++ 05-go/data.go
@@ -1,5 +1,29 @@
 package main
 
+type Stack []string
+
+func (s *Stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) pop() (string, bool) {
+	if s.isEmpty() {
+		return "", false
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element, true
+}
+
+func (s *Stack) peek() string {
+	return (*s)[len(*s)-1]
+}
+
 func SampleState() []string {
 	return []string{"ZN", "MCD", "P"}
 }
--- 05-go/main.go
+++ 05-go/main.go
@@ -5,30 +5,6 @@ import (
 	"strings"
 )
 
-type Stack []string
-
-func (s *Stack) isEmpty() bool {
-	return len(*s) == 0
-}
-
-func (s *Stack) push(str string) {
-	*s = append(*s, str)
-}
-
-func (s *Stack) pop() (string, bool) {
-	if s.isEmpty() {
-		return "", false
-	}
-	index := len(*s) - 1
-	element := (*s)[index]
-	*s = (*s)[:index]
-	return element, true
-}
-
-func (s *Stack) peek() string {
-	return (*s)[len(*s)-1]
-}
-
 func makeState(states []string) []Stack {
 	var result []Stack
 	for _, state := range states {
