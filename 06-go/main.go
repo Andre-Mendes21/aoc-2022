a850b9d2e3fb8777dcabcd69f10ae7175efa8659
U-DESKTOP-D1A8VVO\Andre <a67993@ualg.pt>
2022-12-08 17:20:39 +0000

    Day 6 solution

---
+++ 06-go/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func dataFromFile(filePath string) (dataStr []string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		dataStr = append(dataStr, line)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return dataStr
+}
+
+func hasRepeats(marker string) bool {
+	for i, rn := range marker {
+		if strings.Contains(marker[i+1:], string(rn)) {
+			return true
+		}
+	}
+	return false
+}
+
+func solveBoth(filePath string, n int) (result []int) {
+	dataStr := dataFromFile(filePath)
+	for _, data := range dataStr {
+		for i := 0; i < len(data); i++ {
+			marker := data[i : i+n]
+			if !hasRepeats(marker) {
+				numChar := i + n
+				result = append(result, numChar)
+				break
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	for _, filePath := range os.Args[1:] {
+		fmt.Printf("Input: %s\n", filePath)
+		fmt.Println("Part One:", solveBoth(filePath, 4))
+		fmt.Println("Part Two:", solveBoth(filePath, 14))
+	}
+}
